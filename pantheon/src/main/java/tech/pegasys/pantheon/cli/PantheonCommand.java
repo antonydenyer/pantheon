31cba941d45a9b03b7e4c265bb1794161765d221
Antony Denyer <git@antonydenyer.co.uk>
2019-01-22 09:46:43 +0000

    Squashed commit of the following: #584
    commit fb706ccb36977260f8528500530a4eac34978118
    Author: Puneetha <puneetha17@gmail.com>
    Date:   Fri Jan 18 11:49:57 2019 +0000
    
        Wire up Precompiled contract configuration to Protocol Spec
    
    commit 72ff0961f47ff9a2890191f8385444b91a5d1f9b
    Author: Puneetha <puneetha17@gmail.com>
    Date:   Fri Jan 18 11:09:57 2019 +0000
    
        Add Privacy Cli options

--- pantheon/src/main/java/tech/pegasys/pantheon/cli/PantheonCommand.java
+++ pantheon/src/main/java/tech/pegasys/pantheon/cli/PantheonCommand.java
@@ -27,6 +27,7 @@ import tech.pegasys.pantheon.controller.KeyPairUtil;
 import tech.pegasys.pantheon.controller.PantheonController;
 import tech.pegasys.pantheon.ethereum.core.Address;
 import tech.pegasys.pantheon.ethereum.core.MiningParameters;
+import tech.pegasys.pantheon.ethereum.core.PrivacyParameters;
 import tech.pegasys.pantheon.ethereum.core.Wei;
 import tech.pegasys.pantheon.ethereum.eth.sync.SyncMode;
 import tech.pegasys.pantheon.ethereum.eth.sync.SynchronizerConfiguration;
@@ -453,6 +454,24 @@ public class PantheonCommand implements DefaultCommandValues, Runnable {
   )
   private final Collection<String> accountsWhitelist = null;
 
+  @Option(
+    names = {"--privacy-url"},
+    description = "The URL on which enclave is running "
+  )
+  private final URI privacyUrl = PrivacyParameters.DEFAULT_ORION_URL;
+
+  @Option(
+    names = {"--privacy-public-key-file"},
+    description = "the path to the enclave's public key "
+  )
+  private final File privacyPublicKeyFile = null;
+
+  @Option(
+    names = {"--privacy-enabled"},
+    description = "Set if private transaction should be enabled (default: ${DEFAULT-VALUE})"
+  )
+  private final Boolean privacyEnabled = false;
+
   public PantheonCommand(
       final BlockImporter blockImporter,
       final RunnerBuilder runnerBuilder,
@@ -580,6 +599,7 @@ public class PantheonCommand implements DefaultCommandValues, Runnable {
           .devMode(isDevMode)
           .nodePrivateKeyFile(getNodePrivateKeyFile())
           .metricsSystem(metricsSystem)
+          .privacyParameters(orionConfiguration())
           .build();
     } catch (final InvalidConfigurationException e) {
       throw new ExecutionException(new CommandLine(this), e.getMessage());
@@ -630,6 +650,14 @@ public class PantheonCommand implements DefaultCommandValues, Runnable {
     permissioningConfiguration.setNodeWhitelist(nodesWhitelist);
     permissioningConfiguration.setAccountWhitelist(accountsWhitelist);
     return permissioningConfiguration;
+  }
+
+  private PrivacyParameters orionConfiguration() {
+    final PrivacyParameters privacyParameters = PrivacyParameters.createDefault();
+    privacyParameters.setEnabled(privacyEnabled);
+    privacyParameters.setUrl(privacyUrl.toString());
+    privacyParameters.setPublicKey(privacyPublicKeyFile);
+    return privacyParameters;
   }
 
   private SynchronizerConfiguration buildSyncConfig(final SyncMode syncMode) {
